84e62f98d8193e38a8e32be9f24bddc6926be8d0
OSKAR COMPAINS <oscarcompains@gmail.com>
2015-09-27 18:15:21 +0200

    cambios realizados hasta lo que dan mis conocimientos ahora mismo :(

---
+++ tarea02oskar/src/main/java/org/cuatrovientos/tarea02oskar/App.java
@@ -0,0 +1,39 @@
+package org.cuatrovientos.tarea02oskar;
+
+
+import java.util.Scanner;
+import java.util.Vector;
+
+import org.cuatrovientos.tarea02oskar.Bd;
+import org.cuatrovientos.tarea02oskar.Customer;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		String option = "0";
+		Scanner reader = new Scanner(System.in);
+		Bd bd = new Bd();
+		
+		do {
+			System.out.println("Enter option");
+			option = reader.nextLine();
+			
+			switch (option) {
+			case "1":
+					Vector<Customer> result = bd.selectAll();
+					System.out.println(result.toString());
+				break;
+			case "2":
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "5":
+				break;
+			default:
+				break;
+			}
+		} while (!option.equals("6"));
+	}
+}
